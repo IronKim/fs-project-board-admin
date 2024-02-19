12da29bdeea62acbe65c67f2c222b7f1bdb2fe5a
IronKim <tankokim@gmail.com>
2024-02-19 20:58:26 +0900

    #24 - 어드민 회원 테이블명 교체
    `user_account` -> `admin_account`로 어드민 회원 테이블명 변경.
    통신하려는 게시판 서비스의 회원 도메인명과 이름이 겹친다.

--- src/main/java/com/fs/projectboardadmin/repository/UserAccountRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fs.projectboardadmin.repository;
-
-import com.fs.projectboardadmin.domain.UserAccount;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserAccountRepository extends JpaRepository<UserAccount, String> {
-}
