406f6497a810f2108c664358060db70226c7b2e5
IronKim <tankokim@gmail.com>
2024-02-20 19:34:15 +0900

    #30 - 에러페이지 작성
    스프링 부트에서 지원하는 에러 페이지 등록 방법을 이용하여
    간단하게 4xx, 5xx 에러에 각각 대응하는 에러 페이지를 등록
    스프링부트3 기준 타임리프가 3.1로 업데이트 되면서
    보안상 문제로 #request, #response, #session, and #servletContext를 사용할 수 없게 되었다.
    때문에 @ControllerAdvice 어노테이션을 사용해 모든 컨트롤러에서 발생하는 예외 처리를 관리하는 컨트롤러를 생성 후 ModelAttribute에 현재 status값을 넣어주어 사용하였다.
    참고:
    https://stackoverflow.com/questions/74594544/getrequesturi-is-null-with-netty-and-spring-boot-3
    https://www.thymeleaf.org/doc/articles/thymeleaf31whatsnew.html

--- src/main/java/com/fs/projectboardadmin/controller/GlobalControllerAdvice.java
+++ src/main/java/com/fs/projectboardadmin/controller/GlobalControllerAdvice.java
@@ -1,5 +1,6 @@
 package com.fs.projectboardadmin.controller;
 
+import jakarta.servlet.RequestDispatcher;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -11,4 +12,7 @@ public class GlobalControllerAdvice {
     public String exposeUrl(HttpServletRequest request) {
         return request.getRequestURI();
     }
+
+    @ModelAttribute("status")
+    public Object exposeStatus(HttpServletRequest request) {return request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);}
 }
