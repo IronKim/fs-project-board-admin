b6238ec6bdc4ebec6098c562d7793edea5fefee6
IronKim <tankokim@gmail.com>
2024-02-20 20:44:10 +0900

    #37 - 댓글 관리 페이지의 컨트롤러, 서비스, 뷰 구현

---
+++ src/main/java/com/fs/projectboardadmin/dto/response/ArticleCommentResponse.java
@@ -0,0 +1,26 @@
+package com.fs.projectboardadmin.dto.response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fs.projectboardadmin.dto.ArticleCommentDto;
+import com.fs.projectboardadmin.dto.UserAccountDto;
+
+import java.time.LocalDateTime;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public record ArticleCommentResponse(
+        Long id,
+        UserAccountDto userAccount,
+        String content,
+        LocalDateTime createdAt
+) {
+
+    public static ArticleCommentResponse of(Long id, UserAccountDto userAccount, String content, LocalDateTime createdAt) {
+        return new ArticleCommentResponse(id, userAccount, content, createdAt);
+    }
+
+    public static ArticleCommentResponse of(ArticleCommentDto dto) {
+        return ArticleCommentResponse.of(dto.id(), dto.userAccount(), dto.content(), dto.createdAt());
+    }
+
+}
+
